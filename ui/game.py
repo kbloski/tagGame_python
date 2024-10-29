85ba01fa9cf29a7af4b0991d78fb2f424d6d3206
Kamil Błoński <kblonski02@gmail.com>
2024-10-29 06:06:52 +0100

    Add ViewModel

--- ui/game.py
+++ ui/game.py
@@ -4,3 +4,5 @@ from game.game_manager.main_game import MainGame
 class CreateGame(MainGame, ViewModel):
     def __init__(self):
         super().__init__()
+
+        
--- ui/models/viewModel.py
+++ ui/models/viewModel.py
@@ -1,9 +1,11 @@
 import pygame # type: ignore
 from config.screen import screen
+from config.screen import screen
+from utils.writeUtils import drawSign
 
 class ViewModel:
     def __init__(self):
         pass
 
     def render(self):
-        pass
+        drawSign(screen, "Custom - ViewModel.render(self): function.", [50,50], 25)
