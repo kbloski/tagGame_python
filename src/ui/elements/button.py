6ae80a7946c5eaa949d4038e6722c07e2d6f9ab3
Kamil Błoński <kblonski02@gmail.com>
2025-03-27 23:06:57 +0100

    Update

--- src/ui/elements/button.py
+++ src/ui/elements/button.py
@@ -1,8 +1,32 @@
-# from ...graphics.drawText import CreateSign, createSignSurface
+import pygame
+from ...config.screen import screen
+from ...graphics.drawText import CreateSign, createSignSurface
 
-# class Button: 
-#     def __init__(self, text, width, height):
-#         self.surfaces = 
-#         pass
+class CreateButton: 
+    def __init__(self, 
+                 text = 'button', 
+                 bgColor = '#555555',
+                 position = [0,0], 
+                 width = None, 
+                 height = None ):
+        
+        self.size = [100, 50]
+        self.pos = position
+        # self.size = [width, height]
+        self.bgColor = bgColor
+        self.signSurface = createSignSurface( text )
+        self.surfaces = {
+            'basic' : None,
+            'hover' : None
+        }
+        
+        self._createButtonImgs()
 
-#     def _createButtonImg = 
+    def _createButtonImgs( self ):
+        baseSurface = pygame.Surface( self.size)
+        baseSurface.fill( self.bgColor )
+        self.surfaces['basic'] = baseSurface
+
+    def draw( self ):
+        screen.blit( self.surfaces['basic'],  self.pos)
+
--- src/ui/views/menu.py
+++ src/ui/views/menu.py
@@ -2,9 +2,13 @@ from ...config.screen import screen
 from ...graphics import drawText
 from ..models.viewModel import ViewModel
 
+from ..elements.button import CreateButton
+
 class CreateMenu( ViewModel ):
     def __init__(self):
+        self.startButton = CreateButton()
         pass 
 
     def render(self):
+        self.startButton.draw()
         drawText.drawSign(screen, 'MAIN')
